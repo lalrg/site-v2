3d36c2a1e3ba9109994885778a00d12995a42a8b
Luis Richmond <luis.alex23r@gmail.com>
2023-01-16 14:42:41 -0600

    add tailwind

--- site-nextjs/pages/index.tsx
+++ site-nextjs/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>About</h1>
+      <h1>👋🏾Hi I am Luis</h1>
       <p>I am a problem-solver and enjoy helping people do their jobs more efficiently through the development of applications. I have been working in this field for about 7 years and have had the opportunity to work with a variety of companies, including the Costa Rican government, small businesses in Costa Rica, and global companies with hundreds of employees. I have also worked as a consultant for several North American companies.</p>
       <h2>Professional Summary</h2>
       <p>I have a strong background in software development, with a focus on using technologies such as React, JavaScript, TypeScript, SQL Server, .Net Core, and Python.</p>
